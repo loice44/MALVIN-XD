c5652512c92d20ba62f46ddb00d3a8c6f8b71ba3
loice44 <loicemushata@gmail.com>
2025-08-15 22:23:54 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~gzdWGabQ#igTI_rolXUwUmaPBiwJZOcKpRV4YTZRRlZQ7XeNrEc0",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "263077 193 9084", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
